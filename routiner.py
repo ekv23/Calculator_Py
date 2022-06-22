070e229cdcb46cbb1fb5ed3f9dde11e4ac0fc840
paul <outimer@mail.ru>
2022-06-22 13:41:21 +0600

    routiner fix1

--- routiner.py
+++ routiner.py
@@ -3,6 +3,11 @@ import viewer as vr
 import routiner_real as rr
 import routiner_complex as rc
 
+def frac_trans(value):
+    st = value.split('/')
+    st1 = st[0].split()
+    return f'{st[0].strip()}/{st[1].strip()}' if len(st1) == 1 else f'{st1[0].strip()} {st1[1].strip()}/{st[1].strip()}'
+
 def calc(first_numb, second_numb, numb_oper):
     lr.log_info()
 
@@ -12,8 +17,8 @@ def calc(first_numb, second_numb, numb_oper):
         return rr.calc(first_numb, second_numb, numb_oper)
 
     if vr.is_frac(first_numb) and vr.is_frac(second_numb):
-        first_numb = str(first_numb)
-        second_numb = str(second_numb)
+        first_numb = frac_trans(first_numb)
+        second_numb = frac_trans(second_numb)
         return rr.calc(first_numb, second_numb, numb_oper)
     
     if vr.is_complex(first_numb) and vr.is_complex(second_numb):

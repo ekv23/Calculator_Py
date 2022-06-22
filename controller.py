45f8296c99a5ad6c785ad14278b5b2f1ae0c03ac
paul <outimer@mail.ru>
2022-06-22 11:13:25 +0600

    controller fix1

--- controller.py
+++ controller.py
@@ -2,7 +2,7 @@ import viewer as vr
 import routiner as rt
 
 def button_click(flag):
-    numb_type, first_numb, second_numb, numb_oper = vr.menu_collection(flag)
-    res = rt.calc(numb_type, first_numb, second_numb, numb_oper)
+    first_numb, second_numb, numb_oper = vr.menu_collection(flag)
+    res = rt.calc(first_numb, second_numb, numb_oper)
     vr.view_res(res)
 

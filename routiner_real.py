a58672f8420fb182d80afcb411a2eee6b8938ed5
Aleksandr <alexlexx444@yandex.ru>
2022-06-23 02:04:47 +0500

    interim solution

--- routiner_real.py
+++ routiner_real.py
@@ -1,12 +1,67 @@
-def calc(a,b,oper):     # метод, принимающий аргументами переменные и оператор совершаемый над ними.
-    if type(a) == str: a_real = float(a)
-    if type(b) == str: b_real = float(b)
-    if type(a) == tuple: a_real = float(a[0])
-    if type(b) == tuple: b_real = float(b[0])
+# метод, принимающий аргументами переменные и оператор совершаемый над ними.
+
+from fractions import Fraction
+
+def calc(a,b,oper):
+    # если в полученной строку
+    if type(a) == str and type(a) == str: 
+        if a.find('/') < 0 and b.find('/') < 0:
+            a_real = float(a)
+            b_real = float(b)
+            return operation(a_real, b_real, oper)
+        else:
+            if a[a.find('/') + 1] == '0' or b[b.find('/') + 1] == '0': return 'Error! Division by zero.'
+            else: return parce_fraction_answer(operation_fraction(pars_fraction(a), pars_fraction(b), oper))
+
+    # если получили кортеж
+    if type(a) == tuple and type(b) == tuple: 
+        a_real = float(a[0])
+        b_real = float(b[0])
+        return operation(a_real, b_real, oper)
+
+# принимает два числа с плаваюшей точкой и возвращает ответ по операции
+def operation (n1, n2, oper):
     match oper:
-        case '+': return a_real + b_real
-        case '-': return a_real - b_real
-        case '*': return a_real * b_real
+        case '+': return n1 + n2
+        case '-': return n1 - n2
+        case '*': return n1 * n2
         case '/':
-            if float(b_real) == 0.0: print("Null. Shall. Not. Pa-a-a-ass.\nYou can't divide by zero, sorry, not on my shift.")
-            else: return a_real / b_real
+            if float(n2) == 0.0: 
+                return 'Error!!! Division by zero.'
+            else: 
+                return n1 / n2
+
+# принимает на вход две дроби простого вида a/b и возвращает ответ операции над ними
+def operation_fraction (n1, n2, oper):
+    match oper:
+        case '+': return f'{Fraction(n1).numerator * Fraction(n2).denominator + Fraction(n2).numerator * Fraction(n1).denominator}/{Fraction(n2).denominator * Fraction(n1).denominator}'
+        case '-': return f'{Fraction(n1).numerator * Fraction(n2).denominator - Fraction(n2).numerator * Fraction(n1).denominator}/{Fraction(n2).denominator * Fraction(n1).denominator}'
+        case '*': return f'{Fraction(n1).numerator * Fraction(n2).numerator}/{Fraction(n1).denominator * Fraction(n2).denominator}'
+        case '/': return f'{Fraction(n1).numerator * Fraction(n2).denominator}/{Fraction(n2).numerator * Fraction(n1).denominator}'
+
+# приводим дробь к обычному виду
+def pars_fraction (number):
+    number_slash_index = number.find(' ')
+    frac_num = ''
+    if number_slash_index > 0: int_num = ''        
+    else: return number
+
+    for i in range(number_slash_index):
+        int_num += str(number[i])
+    for i in range(number_slash_index + 1, len(number)):
+        frac_num += str(number[i])
+
+    return f'{Fraction(frac_num).numerator + int(int_num) * Fraction(frac_num).denominator}/{Fraction(frac_num).denominator}'
+
+# выделяем в дроби целочисленную состовляющую (если есть)
+def parce_fraction_answer (number):
+    if Fraction(number).numerator > Fraction(number).denominator:
+        int_num = str(int(Fraction(number).numerator / Fraction(number).denominator))
+        return f'{int_num} {Fraction(number).numerator - int(int_num) * Fraction(number).denominator}/{Fraction(number).denominator}'
+    elif Fraction(number).numerator * -1 > Fraction(number).denominator:
+        int_num = str(int(Fraction(number).numerator / Fraction(number).denominator))
+        return f'{int_num} {(Fraction(number).numerator - int(int_num) * Fraction(number).denominator) * -1}/{Fraction(number).denominator}'
+    else:
+        return number
+
+print(calc('1 1/18', '2 3/4', '/'))

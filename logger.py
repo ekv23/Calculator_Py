6cbf4100c3be0a072c804869244a7248889eff46
Ekaterina <ekaterina.s.velichko@gmail.com>
2022-06-29 15:03:07 +0300

    logger code added

--- logger.py
+++ logger.py
@@ -1,2 +1,12 @@
-#log_info(data)
-#log_error(data)
+from datetime import datetime as dt
+from distutils.log import error
+
+def log_info(data):
+    time = dt.now().strftime('%D %H:%M')
+    with open('log.txt', 'a') as file:
+        file.write(f'{time} info: {data}\n')
+
+def log_error(data):
+    time = dt.now().strftime('%D %H:%M')
+    with open('log.txt', 'a') as file:
+        file.write(f'{time} error: {data}\n')

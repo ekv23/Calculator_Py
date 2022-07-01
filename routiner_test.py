8d5b6af6dec65657eea2048a07b603504af9a6f5
«Kamila» <koskanova@outlook.com>
2022-07-01 14:19:43 +0300

    Edited the test file, updated and changes some of the answers

--- routiner_test.py
+++ routiner_test.py
@@ -7,52 +7,52 @@ from routiner import calc as Calc_num
 def test1_Calc_num():
     """Проверка сложения десятичных дробей в `Calc_num` функции"""
     output = Calc_num('6.75', '8.35', '+')
-    assert output == 15.1
+    assert output == '15.1'
 
 def test2_Calc_num():
     """Проверка сложения десятичных дробей в `Calc_num` функции"""
     output = Calc_num('9.35', '-9.35', '+')
-    assert output == 0.00
+    assert output == '0.0'
 
 def test3_Calc_num():
     """Проверка вычитания десятичных дробей в `Calc_num` функции"""
     output = Calc_num('9.35', '8.35', '-')
-    assert output == 1.00
+    assert output == '1.0'
 
 def test4_Calc_num():
     """Проверка вычитания десятичных дробей в `Calc_num` функции"""
     output = Calc_num('9.35', '9.35', '-')
-    assert output == 0.00
+    assert output == '0.0'
 
 def test5_Calc_num():
     """Проверка умножения десятичных дробей в `Calc_num` функции"""
     output = Calc_num('8.5', '3.61', '*')
-    assert output == 30.685
+    assert output == '30.68'
 
 def test6_Calc_num():
     """Проверка деления десятичных дробей в `Calc_num` функции"""
     output = Calc_num('9.35', '8.35', '/')
-    assert output == 1.1197604790419162
+    assert output == '1.12'
 
 def test7_Calc_num():
     """Проверка деления десятичных дробей в `Calc_num` функции"""
     output = Calc_num('6.2', '3.1', '/')
-    assert output == 2.00
+    assert output == '2.0'
 
 def test8_Calc_num():
     """Проверка деления десятичных дробей в `Calc_num` функции"""
     output = Calc_num('9.35', '9.35', '/')
-    assert output == 1.00
+    assert output == '1.0'
 
 def test9_Calc_num():
     """Проверка деления на ноль в `Calc_num` функции"""
     output = Calc_num('9.35', '0', '/')
-    assert output == None
+    assert output == 'Division by zero'
 
 def test10_Calc_num():
     """Проверка деления ноля в `Calc_num` функции"""
     output = Calc_num('0', '123.5', '/')
-    assert output == 0.00
+    assert output == '0.0'
 
 
 
@@ -62,67 +62,56 @@ def test11_Calc_num():
     """Проверка сложения смешанных дробей в `Calc_num` функции"""
     output = Calc_num('4 3/5', '1/5', '+')
     assert output == '4 4/5'
-# Тест 11 не пройден.
-# ValueError: could not convert string to float: '4 3/5'
 
 def test12_Calc_num():
     """Проверка сложения смешанных дробей в `Calc_num` функции"""
     output = Calc_num('4 3/5', '-3/5', '+')
     assert output == '4' 
-    # output == 4  Результатом будет float или string?
 # Тест 12 не пройден.
-# AssertionError: assert 'given numbers have different types or inappropriate format' == '4'
+# AssertionError: assert '4 0/1' == '4'
+# Дробная часть отображается как '0/1'
 
 def test13_Calc_num():
     """Проверка сложения смешанных дробей в `Calc_num` функции"""
     output = Calc_num('4 3/5', '2/5', '+')
-    assert output == '5' # output == 5.00 или '4 5/5'
-# Результатом будет float или string?
+    assert output == '5' 
 # Тест 13 не пройден.
-# ValueError: could not convert string to float: '4 3/5'
+# AssertionError: assert '5 0/1' == '5'
+# Дробная часть отображается как '0/1'
 
 def test14_Calc_num():
     """Проверка вычитания обыкновенных дробей в `Calc_num` функции"""
     output = Calc_num('13/15', '1/5', '-')
-    assert output == '2/3' # сокращение дроби '10/15'
-# Тест 14 не пройден.
-# ValueError: could not convert string to float: '13/15'
+    assert output == '2/3'
 
 def test15_Calc_num():
     """Проверка вычитания обыкновенных дробей в `Calc_num` функции"""
     output = Calc_num('10/15', '2/3', '-')
-    assert output == '0' # или число будет сразу типа float 0.00?
-# Тест 15 не пройден.
-# ValueError: could not convert string to float: '10/15'
+    assert output == '0'
 
 def test16_Calc_num():
     """Проверка умножения дробей в `Calc_num` функции"""
     output = Calc_num('2 3/4', '1/5', '*')
     assert output == '11/20'
-# Тест 16 не пройден.
-# ValueError: could not convert string to float: '2 3/4'
 
 def test17_Calc_num():
     """Проверка умножения дробей в `Calc_num` функции"""
     output = Calc_num('2 3/4', '0/5', '*')
-    assert output == '0' # или число будет сразу типа float 0.00?
-# Тест 17 не пройден.
-# ValueError: could not convert string to float: '2 3/4'
+    assert output == '0'
 
 def test18_Calc_num():
     """Проверка деления дробей в `Calc_num` функции"""
     output = Calc_num('3/4', '1/2', '/')
     assert output == '1 1/2'
-# Тест 18 не пройден.
-# ValueError: could not convert string to float: '3/4'
 
 
 def test19_Calc_num():
     """Проверка деления дробей в `Calc_num` функции"""
     output = Calc_num('3/4', '0/2', '/')
-    assert output == None
+    assert output == 'Division by zero'
 # Тест 19 не пройден.
-# ValueError: could not convert string to float: '3/4'
+# Не возращает строку 'Division by zero'. 
+# Просто отображается ошибка 'ZeroDivisionError: division by zero'. 
 
 
 # Проверка комлексных чисел
@@ -130,53 +119,57 @@ def test19_Calc_num():
 def test20_Calc_num():
     """Проверка сложения комплексных чисел в `Calc_num` функции"""
     output = Calc_num('8+2j', '7-4j', '+')
-    assert output == (15-2j)
+    assert output == '(15-2j)'
 
 def test21_Calc_num():
     """Проверка сложения комплексных чисел в `Calc_num` функции"""
     output = Calc_num('8+2j', '-8-4j', '+')
-    assert output == (-2j)
+    assert output == '-2j'
 
 def test22_Calc_num():
     """Проверка сложения комплексных чисел в `Calc_num` функции"""
     output = Calc_num('8+4j', '-8-4j', '+')
-    assert output == (0) # или число будет сразу типа float 0.00?
+    assert output == '0j'
 
 def test23_Calc_num():
     """Проверка вычитания комплексных чисел в `Calc_num` функции"""
     output = Calc_num('-2+j', '7-4j', '-')
-    assert output == (-9+5j)
+    assert output == '(-9+5j)'
 
 def test24_Calc_num():
     """Проверка вычитания комплексных чисел в `Calc_num` функции"""
     output = Calc_num('-2-4j', '7-4j', '-')
-    assert output == (-9) # или число будет сразу типа float -9.00?
+    assert output == '(-9+0j)'
 
 def test25_Calc_num():
     """Проверка вычитания комплексных чисел в `Calc_num` функции"""
     output = Calc_num('-2-4j', '-2-4j', '-')
-    assert output == (0) # или число будет сразу типа float 0.00?
+    assert output == '0j'
 
 def test26_Calc_num():
     """Проверка умножения комплексных чисел в `Calc_num` функции"""
     output = Calc_num('-2+2j', '7-4j', '*')
     assert output == (-2+2j)*(7-4j)
+#  Тест 26 не пройден.
+#  Каким-то образом ответом вычислений является (-6+22j)
+#  При раскрытии скобок получается (-2+2j)*(7-4j) = -14 + 22j - 8j^2 
 
 def test27_Calc_num():
     """Проверка умножения комплексных чисел в `Calc_num` функции"""
-    output = Calc_num('-2+2j', '0j', '*')
-    assert output == (0)
+    output = Calc_num('-2+2j', '0', '*')
+    assert output == '0j'
+#  Тест 27 не пройден.
+#  Код получает ответ 'given numbers have different types or inappropriate format',
+#  Ответом может быть только '0'
 
 def test28_Calc_num():
     """Проверка деления комплексных чисел в `Calc_num` функции"""
     output = Calc_num('-2+2j', '7-4j', '/')
     assert output == (-2+2j)/(7-4j) 
-# Тест 28 не пройден. не получается получить результат, постоянно высвечивается ошибка
-# TypeError: float() argument must be a string or a real number, not 'complex'
+# Тест 28 не пройден. 
+# Результатом вычислений является'(-0.3384615384615385+0.09230769230769233j)'
 
 def test29_Calc_num():
     """Проверка деления комплексных чисел в `Calc_num` функции"""
     output = Calc_num('-2+2j', '-2+2j', '/')
-    assert output == (1) # или число будет сразу типа float 1.00?
-# Тест 29 не пройден.
-# TypeError: float() argument must be a string or a real number, not 'complex'
+    assert output == '(1-0j)'
